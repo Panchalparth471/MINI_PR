06b945f0198ce1ae958b10e181c7f76c6e758be4
nilaygit-10721 <reallynilay123@gmail.com>
2025-04-18 14:34:01 +0530

    Update index.js

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -15,8 +15,8 @@ const PORT = process.env.PORT || 4000;
 app.use(
   cors({
     origin: [
-      "https://mini-a1ejs8d6f-nilays-projects-c34e98cc.vercel.app/",
-      "https://mini-pr-ivory.vercel.app/"// Your Vite development server
+      "https://mini-a1ejs8d6f-nilays-projects-c34e98cc.vercel.app",
+      "https://mini-pr-ivory.vercel.app"// Your Vite development server
     ],
     credentials: true,
   })
